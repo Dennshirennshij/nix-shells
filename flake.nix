89e70398b163ddb50880a7317d074d9d09e088ee
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2025-08-20 16:42:38 +0200

    Added rust-esp32 shell

--- flake.nix
+++ flake.nix
@@ -1,20 +1,16 @@
 {
   description = "A collection of dev shells for various languages";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: 
-  let 
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.x86_64-linux = {
+        rust = import ./shells/rust { inherit pkgs; };
+        rust-esp32 = import ./shells/rust-esp32 { inherit pkgs; };
+      };
     };
-    in
-    {
-    devShells.x86_64-linux = {
-      rust = import ./shells/rust { inherit pkgs; };
-    };
-  };
 }
