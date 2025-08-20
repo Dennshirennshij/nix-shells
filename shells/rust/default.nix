82f68bfd9b73bc4ab1e229f8899d44dd7b3cc6a6
Dennshirennshij <111683708+Dennshirennshij@users.noreply.github.com>
2025-08-20 13:24:28 +0200

    Added lock, Rust in a fishied-for-now state

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -1,8 +1,43 @@
 { pkgs }:
-pkgs.mkShell {
-  name = "rust";
-  buildInputs = [
-    pkgs.rustup
+let
+
+  nativeBuildInputs = with pkgs; [ rustup gcc pkg-config ];
+  buildInputs = with pkgs; [
+    # misc
+    openssl
+
+    # x11 libs
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libX11
+
+    # wayland
+    wayland
+
+    # GUI libs
+    libxkbcommon
+    libGL
+    fontconfig
   ];
+  libaryPath = pkgs.lib.makeLibraryPath buildInputs;
+
+in pkgs.mkShell {
+  name = "rust-stable";
+
+  inherit nativeBuildInputs buildInputs;
+
+  LD_LIBRARY_PATH = "${libaryPath}:$LD_LIBRARY_PATH";
+
+  shellHook = ''
+    echo "🚀 Development shell for generic Rust development"
+
+    # Initialize Rust via rustup if missing
+    if ! command -v rustc &>/dev/null; then
+      echo "📦 Installing stable Rust toolchain via rustup..."
+      rustup install stable
+      rustup default stable
+    fi
+  '';
 }
 
